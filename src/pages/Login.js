93a1962aa0c81b6efa76113db281df237788416a
atok624 <84607674+atok624@users.noreply.github.com>
2023-07-19 09:55:22 +0000

    Add login page

---
+++ src/pages/Login.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    console.log('Login:', email, password);
+    // Reset form
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <section className="vh-100" style={{ backgroundColor: '#9A616D' }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+            <div className="card" style={{ borderRadius: '1rem' }}>
+              <div className="row g-0">
+                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                    alt="login form"
+                    className="img-fluid"
+                    style={{ borderRadius: '1rem 0 0 1rem' }}
+                  />
+                </div>
+                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-5 text-black">
+                    <form onSubmit={handleLogin}>
+                      <div className="d-flex align-items-center mb-3 pb-1">
+                        <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }} />
+                        <span className="h1 fw-bold mb-0">Logo</span>
+                      </div>
+                      <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>
+                        Sign into your account
+                      </h5>
+                      <div className="form-outline mb-4">
+                        <input
+                          type="email"
+                          id="form2Example17"
+                          className="form-control form-control-lg"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label className="form-label" htmlFor="form2Example17">
+                          Email address
+                        </label>
+                      </div>
+                      <div className="form-outline mb-4">
+                        <input
+                          type="password"
+                          id="form2Example27"
+                          className="form-control form-control-lg"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label className="form-label" htmlFor="form2Example27">
+                          Password
+                        </label>
+                      </div>
+                      <div className="pt-1 mb-4">
+                        <button className="btn btn-dark btn-lg btn-block" type="submit">
+                          Login
+                        </button>
+                      </div>
+                      <a className="small text-muted" href="#!">
+                        Forgot password?
+                      </a>
+                      <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>
+                        Don&apos;t have an account?
+                        {' '}
+                        <a href="#!" style={{ color: '#393f81' }}>
+                          Register here
+                        </a>
+                      </p>
+                      <a href="#!" className="small text-muted">
+                        Terms of use.
+                      </a>
+                      <a href="#!" className="small text-muted">
+                        Privacy policy
+                      </a>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
