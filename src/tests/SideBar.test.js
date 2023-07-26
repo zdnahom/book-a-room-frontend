4e64667299fdfb594ef9ae6a638a9dfb90394d6e
atok624 <84607674+atok624@users.noreply.github.com>
2023-07-26 13:29:46 +0000

    Add test for Sidebar

---
+++ src/tests/SideBar.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import SideBar from '../components/SideBar';
+
+test('renders navigation links', () => {
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>,
+  );
+
+  const roomsLinks = screen.queryAllByText(/Rooms/i);
+
+  // Assert that there is at least one link with "Rooms" text
+  expect(roomsLinks.length).toBeGreaterThan(0);
+
+  // Add other assertions as needed for the other links
+  // ...
+});
